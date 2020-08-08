e8cb44e37f001aaf1c0cb99a28d09228bc201537
siuming <siumingdev@gmail.com>
2020-08-09 05:26:02 +0800

    add Spring starter

---
+++ src/main/java/dev/siuming/graphqljavademo/GraphqlJavaDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.siuming.graphqljavademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlJavaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlJavaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/siuming/graphqljavademo/GraphqlJavaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.siuming.graphqljavademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlJavaDemoApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
